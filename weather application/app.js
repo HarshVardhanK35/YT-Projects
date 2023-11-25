3850b132388df01ca4c862a377ac5995ff9102d4
HarshVardhanK35 <kasa.saiharshavardhan@gmail.com>
2023-11-26 01:33:21 +0530

    Files Updated

--- weather application/app.js
+++ weather application/app.js
@@ -15,16 +15,17 @@ const getWeather = (city) => {
       const res = response.json().then((data) => {
         // cloud_pct.innerHTML = data.cloud_pct;
         temp.innerHTML = data.temp;
+        tempHead.innerHTML = data.temp;
         feels_like.innerHTML = data.feels_like;
         humidity.innerHTML = data.humidity;
+        humidityHead.innerHTML = data.humidity;
         min_temp.innerHTML = data.min_temp;
         max_temp.innerHTML = data.max_temp;
         wind_speed.innerHTML = data.wind_speed;
+        windHead.innerHTML = data.wind_speed;
         wind_degrees.innerHTML = data.wind_degrees;
         sunrise.innerHTML = data.sunrise;
         sunset.innerHTML = data.sunset;
-
-        // console.log(data);
       });
     } catch (error) {
       console.error(error);
@@ -39,9 +40,69 @@ sub.addEventListener("click", (e) => {
   getWeather(city.value);
 });
 
-function showTemp(){
-  let dropMenuCity = document.getElementById('dropCity')
-  let inpCity = document.getElementById('city');
+function showTemp() {
+  let dropMenuCity = document.getElementById("dropCity");
+  let inpCity = document.getElementById("city");
   inpCity.value = dropMenuCity.innerText;
-  alert('Updated Search Box... Click on the Search Button')
+  alert("Updated Search Box... Click on the Search Button");
 }
+
+function showTemp1() {
+  let dropMenuCity = document.getElementById("dropCity1");
+  let inpCity = document.getElementById("city");
+  inpCity.value = dropMenuCity.innerText;
+  alert("Updated Search Box... Click on the Search Button");
+}
+
+function showTemp2() {
+  let dropMenuCity = document.getElementById("dropCity2");
+  let inpCity = document.getElementById("city");
+  inpCity.value = dropMenuCity.innerText;
+  alert("Updated Search Box... Click on the Search Button");
+}
+
+function showTemp3() {
+  let dropMenuCity = document.getElementById("dropCity3");
+  let inpCity = document.getElementById("city");
+  inpCity.value = dropMenuCity.innerText;
+  alert("Updated Search Box... Click on the Search Button");
+}
+
+function showTemp4() {
+  let dropMenuCity = document.getElementById("dropCity4");
+  let inpCity = document.getElementById("city");
+  inpCity.value = dropMenuCity.innerText;
+  alert("Updated Search Box... Click on the Search Button");
+}
+
+const japData = async () => {
+  const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=japan`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "9f805c7051msh18d5165c6b1fb0ap180900jsnf4d233bace08",
+      "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+    },
+  };
+  try {
+    const response = await fetch(url, options);
+    const res = response.json().then((data) => {
+
+      console.log(data);
+
+      japTemp.innerHTML = data.temp;
+      japMaxTemp.innerHTML = data.max_temp;
+      japMinTemp.innerHTML = data.min_temp;
+      japHum.innerHTML = data.humidity;
+      japFeel.innerHTML = data.feels_like;
+      japRise.innerHTML = data.sunrise;
+      japSet.innerHTML = data.sunset;
+      japDegree.innerHTML = data.wind_degrees;
+      japSpeed.innerHTML = data.wind_speed;
+
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+japData()
